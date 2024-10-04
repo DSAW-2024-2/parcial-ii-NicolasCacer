996c83d513439a34b4d5c328b6367b70402dfe29
Nicolas <njcaceres04@gmail.com>
2024-10-04 07:31:44 -0500

    added initial code

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+const PORT = 3000;
 
-fetch ("https://open-meteo.com/")
+const loginRoute = require("./routes/login");
+const weatherRoute = require("./routes/weather");
+const authCookie = require("./middleware/jwtAuthToken");
+
+app.use("/login", loginRoute);
+app.use("/weather",authCookie, weatherRoute);
+
+app.use((req, res) => {
+    res.status(404).json({ message: "Ruta no encontrada" });
+  });
+
+app.listen(PORT,()=>{console.log(`Server running on http://localhost:${PORT}`)});
+module.exports = app;
