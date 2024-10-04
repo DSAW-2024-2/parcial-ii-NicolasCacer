f59eac679c2a716eba07c7d32dccae0d525521c2
Nicolas <njcaceres04@gmail.com>
2024-10-04 08:15:16 -0500

    deleted all

--- routes/login.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const authControllers = require("../controllers/login");
-const router = express.Router();
-
-router.post("/", authControllers.login);
-
-module.exports = router;
